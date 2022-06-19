35a7283e832122450fda3dd3ff588f7dcf776340
mongche <mondaydarknight@gmail.com>
2022-06-19 14:27:35 +0800

    style: remove redundant newline

--- internal/httprange/range.go
+++ internal/httprange/range.go
@@ -17,23 +17,17 @@ type ContentRange struct {
 }
 
 // Get the length the file resumed to upload.
-func (cr *ContentRange) Length() int64 {
-	return cr.End - cr.Start + 1
-}
+func (cr *ContentRange) Length() int64 { return cr.End - cr.Start + 1 }
 
 // Get the current number of times the file resumed to upload.
-func (cr *ContentRange) CurrentPart() int64 {
-	return (cr.Start / (cr.End - cr.Start)) + 1
-}
+func (cr *ContentRange) CurrentPart() int64 { return (cr.Start / (cr.End - cr.Start)) + 1 }
 
 // Get all parts the file resumed to upload.
 func (cr *ContentRange) Parts() int64 {
 	remainder := 0
-
 	if cr.Size%cr.Length() > 0 {
 		remainder = 1
 	}
-
 	return cr.Size/cr.Length() + int64(remainder)
 }
 
@@ -46,29 +40,22 @@ func ParseRange(s string, size int64) ([]Range, error) {
 	if s == "" {
 		return nil, nil
 	}
-
 	const b = "bytes="
-
 	if !strings.HasPrefix(s, b) {
 		return nil, fmt.Errorf("invalid range header %q", s)
 	}
-
 	var ranges []Range
-
 	for _, ra := range strings.Split(s[len(b):], ",") {
 		ra = strings.TrimSpace(ra)
 		if ra == "" {
 			continue
 		}
-
 		i := strings.Index(ra, "-")
 		if i < 0 {
 			return nil, fmt.Errorf("invalid range header %q", s)
 		}
-
 		var r Range
 		start, end := strings.TrimSpace(ra[:i]), strings.TrimSpace(ra[i+1:])
-
 		if start == "" {
 			i, err := strconv.ParseInt(end, 10, 64)
 			if err != nil {
@@ -77,7 +64,6 @@ func ParseRange(s string, size int64) ([]Range, error) {
 			if i > size {
 				i = size
 			}
-
 			r.Start = size - i
 			r.Length = size - r.Start
 		} else {
@@ -87,7 +73,6 @@ func ParseRange(s string, size int64) ([]Range, error) {
 			}
 
 			r.Start = i
-
 			if end == "" {
 				r.Length = size - r.Start
 			} else {
@@ -103,56 +88,41 @@ func ParseRange(s string, size int64) ([]Range, error) {
 		}
 		ranges = append(ranges, r)
 	}
-
 	return ranges, nil
 }
 
 func ParseContentRange(s string) (*ContentRange, error) {
 	const b = "bytes "
-
-	if len(s) == 0 {
-		return nil, errors.New("no Content-Range header")
+	if s == "" {
+		return nil, nil
 	}
-
 	if !strings.HasPrefix(s, b) {
 		return nil, errors.New("invalid unit of Content-Range header")
 	}
-
 	r := strings.Split(s[len(b):], "/")
-
 	if len(r) != 2 {
 		return nil, errors.New("invalid size of Content-Range header")
 	}
-
 	size, err := strconv.ParseInt(strings.TrimSpace(r[1]), 10, 64)
-
 	if err != nil {
 		return nil, errors.New("cannot parse size of Content-Range header")
 	}
-
 	r = strings.Split(r[0], "-")
-
 	if len(r) != 2 {
 		return nil, errors.New("cannot parse Content-Range header, expected format \"start-end\"")
 	}
-
 	start, err := strconv.ParseInt(strings.TrimSpace(r[0]), 10, 64)
-
 	if err != nil {
 		return nil, errors.New("cannot parse start of Content-Range header")
 	}
-
 	end, err := strconv.ParseInt(strings.TrimSpace(r[1]), 10, 64)
-
 	if err != nil {
 		return nil, errors.New("cannot parse end of Content-Range header")
 	}
-
 	cr := &ContentRange{
 		Start: start,
 		End:   end,
 		Size:  size,
 	}
-
 	return cr, nil
 }
