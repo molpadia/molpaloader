452507dfc9a9a31ce385e3334b890cb6cc24f0b5
mongche <mondaydarknight@gmail.com>
2022-06-19 13:24:47 +0800

    refactor: divide codebase into ddd domain and infrastructure

--- internal/app/controller.go
+++ internal/app/controller.go
@@ -6,99 +6,66 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
 	"strconv"
-	"strings"
-	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	"github.com/molpadia/molpastream/internal/domain/entity"
 	"github.com/molpadia/molpastream/internal/httprange"
+	"github.com/molpadia/molpastream/internal/infrastructure"
+	"github.com/molpadia/molpastream/internal/infrastructure/persistence"
 )
 
 const (
-	MinUploadChunkSize = 256 << 10
-	MaxUploadChunkSize = 5 << 20
+	minUploadChunkSize = 256 << 10
+	maxUploadChunkSize = 10 << 20
 )
 
 // Create a new video.
 func createVideo(w http.ResponseWriter, r *http.Request) error {
-	var (
-		data     VideoRequest
-		uploadId string
-	)
+	var data VideoRequest
 
 	if err := parseJSON(w, r, &data); err != nil {
 		return fmt.Errorf("cannot parse JSON from request body: %v", err)
 	}
-	if r.Header.Get("X-Upload-Content-Length") == "" {
-		return &AppError{http.StatusBadRequest, "X-Upload-Content-Length header must be required"}
-	}
 	if r.Header.Get("X-Upload-Content-Type") == "" {
 		return &AppError{http.StatusBadRequest, "X-Upload-Content-Type header must be required"}
 	}
-
-	id := uuid.New().String()
-	sess := session.Must(session.NewSession())
-	uploader := s3manager.NewUploader(sess)
-	metadata := map[string]string{
-		"title":       data.Metadata.Title,
-		"description": data.Metadata.Description,
-		"length":      r.Header.Get("X-Upload-Content-Length"),
-		"tags":        strings.Join(data.Metadata.Tags, ","),
-		"type":        r.Header.Get("X-Upload-Content-Type"),
+	size, err := strconv.ParseInt(r.Header.Get("X-Upload-Content-Length"), 10, 64)
+	if err != nil {
+		return &AppError{http.StatusBadRequest, "X-Upload-Content-Length header must be required"}
 	}
-
+	sess := session.Must(session.NewSession())
+	uploader := infrastructure.NewUploader(sess)
+	repo := persistence.NewVideoRepository(sess)
+	// Create a new video entity for persistence data store.
+	video := entity.NewVideo(
+		uuid.New().String(),
+		data.Title,
+		data.Description,
+		r.Header.Get("X-Upload-Content-Type"),
+		size,
+		data.Tags,
+		data.Metadata,
+	)
 	switch r.URL.Query().Get("uploadType") {
 	case "media":
 	case "resumable":
-		// Initiates a multipart upload and return an upload ID from AWS S3.
-		out, err := uploader.S3.CreateMultipartUpload(&s3.CreateMultipartUploadInput{
-			Bucket:   aws.String(os.Getenv("AWS_S3_VOD_BUCKET")),
-			Key:      aws.String(id),
-			Metadata: aws.StringMap(metadata),
-		})
+		uploadId, err := uploader.CreateMultipart(video.Id)
 		if err != nil {
 			return fmt.Errorf("failed to create multipart upload: %v", err)
 		}
-		uploadId = *out.UploadId
+		video.NewUpload(uploadId)
 	default:
 		return &AppError{http.StatusBadRequest, "Invalid upload type"}
 	}
-
-	// Save the multipart file information to the persistence data store.
-	db := dynamodb.New(sess)
-	_, err := db.PutItem(&dynamodb.PutItemInput{
-		Item: map[string]*dynamodb.AttributeValue{
-			"Id": {S: aws.String(id)},
-			"Metadata": {
-				M: map[string]*dynamodb.AttributeValue{
-					"Title":       {S: aws.String(metadata["title"])},
-					"Description": {S: aws.String(metadata["description"])},
-					"Length":      {N: aws.String(metadata["length"])},
-					"Tags":        {S: aws.String(metadata["tags"])},
-					"Type":        {S: aws.String(metadata["type"])},
-				},
-			},
-			"UploadId": {
-				S: aws.String(uploadId),
-			},
-			"CreatedAt": {
-				N: aws.String(strconv.FormatInt(time.Now().Unix(), 10)),
-			},
-		},
-		TableName: aws.String(os.Getenv("AWS_DB_VOD_NAME")),
-	})
+	// Save the multipart file information to the persistence.
+	err = repo.Save(video)
 	if err != nil {
 		return fmt.Errorf("failed to save data to dynamodb: %v", err)
 	}
-
-	return replyJSON(w, VideoResponse{id}, http.StatusOK)
+	return replyJSON(w, VideoResponse{video.Id}, http.StatusOK)
 }
 
 // Upload the video to the remote storage.
@@ -107,28 +74,26 @@ func uploadVideo(w http.ResponseWriter, r *http.Request) error {
 	if id == "" {
 		return &AppError{http.StatusBadRequest, "video ID must be required"}
 	}
-	length, err := strconv.ParseInt(r.Header.Get("Content-Length"), 10, 64)
+	// Get the partial size of video upload.
+	size, err := strconv.ParseInt(r.Header.Get("Content-Length"), 10, 64)
 	if err != nil {
 		return &AppError{http.StatusBadRequest, fmt.Sprintf("cannot parse Content-Length header: %v", err)}
 	}
-	if length < MinUploadChunkSize || length > MaxUploadChunkSize {
-		return &AppError{http.StatusBadRequest, fmt.Sprintf("size must between %d and %d bytes", MinUploadChunkSize, MaxUploadChunkSize)}
+	if size < minUploadChunkSize || size > maxUploadChunkSize {
+		return &AppError{http.StatusBadRequest, fmt.Sprintf("size must between %d and %d bytes", minUploadChunkSize, maxUploadChunkSize)}
 	}
-	if length%MinUploadChunkSize > 0 {
-		return &AppError{http.StatusBadRequest, fmt.Sprintf("size must be the multiple of %d bytes", MinUploadChunkSize)}
+	if size%minUploadChunkSize > 0 {
+		return &AppError{http.StatusBadRequest, fmt.Sprintf("size must be the multiple of %d bytes", minUploadChunkSize)}
 	}
-
 	var cr *httprange.ContentRange
 	sess := session.Must(session.NewSession())
-	db := dynamodb.New(sess)
-	resp, err := db.GetItem(&dynamodb.GetItemInput{
-		Key:       map[string]*dynamodb.AttributeValue{"Id": {S: aws.String(id)}},
-		TableName: aws.String(os.Getenv("AWS_DB_VOD_NAME")),
-	})
+	uploader := infrastructure.NewUploader(sess)
+	repo := persistence.NewVideoRepository(sess)
+	video, err := repo.GetById(id)
 	if err != nil {
 		return fmt.Errorf("failed to retrieve the video file from DynamoDB: %v", err)
 	}
-	if len(resp.Item) == 0 {
+	if video == nil {
 		return &AppError{http.StatusNotFound, "video ID does not exist"}
 	}
 	// Parse the Content-Range header for resumable upload.
@@ -137,32 +102,43 @@ func uploadVideo(w http.ResponseWriter, r *http.Request) error {
 		if err != nil {
 			return fmt.Errorf("cannot parse Content-Range header: %v", err)
 		}
-		if length != cr.Length() {
+		if cr.Length() != size {
 			return &AppError{http.StatusBadRequest, "invalid length of Content-Range header"}
 		}
-		if strconv.FormatInt(cr.Size, 10) != *resp.Item["Metadata"].M["Length"].N {
+		if cr.Size != video.Size {
 			return &AppError{http.StatusBadRequest, "invalid size of Content-Range header"}
 		}
 	}
-	uploader := s3manager.NewUploader(sess)
-	body := http.MaxBytesReader(w, r.Body, MaxUploadChunkSize)
+	buf := new(bytes.Buffer)
+	body := http.MaxBytesReader(w, r.Body, maxUploadChunkSize)
+	if _, err = io.Copy(buf, body); err != nil {
+		return fmt.Errorf("failed to write binary data: %v", err)
+	}
 	// Upload the video file by the given upload type.
 	// - media: Simple upload. Use this type to quickly transfer small media file to the remote storage.
 	// - resumable: Resumable upload. Use this type for large files when there's a high chance fo network interruption.
 	switch r.URL.Query().Get("uploadType") {
 	case "media":
-		if err := simpleUpload(id, body, uploader); err != nil {
-			return err
+		err = uploader.SimpleUpload(id, buf.Bytes())
+		if err != nil {
+			return fmt.Errorf("failed to upload video to S3: %v", err)
 		}
 	case "resumable":
-		completed, err := resumableUpload(id, *resp.Item["UploadId"].S, body, cr, uploader)
+		part, err := uploader.UploadPart(id, video.Upload.Id, buf.Bytes(), cr.Size, cr.CurrentPart())
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to partial upload to S3, %v", err)
+		}
+		video.AddUploadPart(part)
+		if err = repo.Save(video); err != nil {
+			return fmt.Errorf("failed to save data to dynamodb: %v", err)
 		}
 		// Respond to the client if the upload was not completed.
-		if !completed {
+		if len(video.Upload.Parts) < int(cr.Parts()) {
 			w.WriteHeader(http.StatusPartialContent)
 			return nil
+		}
+		if err = uploader.CompleteMultipart(id, video.Upload.Id, video.Upload.Parts); err != nil {
+			return fmt.Errorf("failed to complete multipart upload: %v", err)
 		}
 	default:
 		return &AppError{http.StatusBadRequest, "Invalid upload type"}
@@ -170,74 +146,6 @@ func uploadVideo(w http.ResponseWriter, r *http.Request) error {
 	// Respond in success when the given file has been uploaded.
 	w.WriteHeader(http.StatusOK)
 	return nil
-}
-
-// Upload an entire file to the remote storage.
-func simpleUpload(id string, body io.Reader, uploader *s3manager.Uploader) error {
-	buf := new(bytes.Buffer)
-	_, err := io.Copy(buf, body)
-	if err != nil {
-		return fmt.Errorf("failed to write binary data: %v", err)
-	}
-	_, err = uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(os.Getenv("AWS_S3_VOD_BUCKET")),
-		Key:    aws.String(id),
-		Body:   bytes.NewReader(buf.Bytes()),
-	})
-	if err != nil {
-		return fmt.Errorf("failed to upload video to S3: %v", err)
-	}
-	return nil
-}
-
-// Resumable upload the file into chunks.
-func resumableUpload(id, uploadId string, body io.Reader, cr *httprange.ContentRange, uploader *s3manager.Uploader) (bool, error) {
-	buf := new(bytes.Buffer)
-	written, err := io.Copy(buf, body)
-	if err != nil {
-		return false, fmt.Errorf("failed to write binary data: %v", err)
-	}
-	_, err = uploader.S3.UploadPart(&s3.UploadPartInput{
-		Body:          bytes.NewReader(buf.Bytes()),
-		Bucket:        aws.String(os.Getenv("AWS_S3_VOD_BUCKET")),
-		ContentLength: aws.Int64(written),
-		Key:           aws.String(id),
-		PartNumber:    aws.Int64(cr.CurrentPart()),
-		UploadId:      aws.String(uploadId),
-	})
-	if err != nil {
-		return false, fmt.Errorf("failed to partial upload to S3, %v", err)
-	}
-	out, err := uploader.S3.ListParts(&s3.ListPartsInput{
-		Bucket:   aws.String(os.Getenv("AWS_S3_VOD_BUCKET")),
-		Key:      aws.String(id),
-		UploadId: aws.String(uploadId),
-	})
-	if err != nil {
-		return false, fmt.Errorf("failed to list multipart upload: %v", err)
-	}
-	if len(out.Parts) < int(cr.Parts()) {
-		return false, nil
-	}
-	var parts []*s3.CompletedPart
-	for _, part := range out.Parts {
-		parts = append(parts, &s3.CompletedPart{
-			ETag:       part.ETag,
-			PartNumber: part.PartNumber,
-		})
-	}
-	_, err = uploader.S3.CompleteMultipartUpload(&s3.CompleteMultipartUploadInput{
-		Bucket: aws.String(os.Getenv("AWS_S3_VOD_BUCKET")),
-		Key:    aws.String(id),
-		MultipartUpload: &s3.CompletedMultipartUpload{
-			Parts: parts,
-		},
-		UploadId: aws.String(uploadId),
-	})
-	if err != nil {
-		return false, fmt.Errorf("failed to complete multipart upload: %v", err)
-	}
-	return true, nil
 }
 
 // Parse incoming request body as JSON object.
---
+++ internal/infrastructure/uploader.go
@@ -0,0 +1,77 @@
+package infrastructure
+
+import (
+	"bytes"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/molpadia/molpastream/internal/domain/entity"
+)
+
+var bucket = os.Getenv("AWS_S3_VOD_BUCKET")
+
+type Uploader struct {
+	s3Uploader *s3manager.Uploader
+}
+
+func NewUploader(sess *session.Session) *Uploader {
+	return &Uploader{s3manager.NewUploader(sess)}
+}
+
+// Initiates a multipart upload and return an upload ID from remote AWS S3 storage.
+func (u *Uploader) CreateMultipart(key string) (string, error) {
+	out, err := u.s3Uploader.S3.CreateMultipartUpload(&s3.CreateMultipartUploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	return *out.UploadId, err
+}
+
+// Mark the multipart upload as completd for the remote AWS S3 storage.
+func (u *Uploader) CompleteMultipart(key, uploadId string, parts []*entity.Part) error {
+	var fileParts []*s3.CompletedPart
+	for _, part := range parts {
+		fileParts = append(fileParts, &s3.CompletedPart{
+			ETag:       aws.String(part.ETag),
+			PartNumber: aws.Int64(part.PartNumber),
+		})
+	}
+	_, err := u.s3Uploader.S3.CompleteMultipartUpload(&s3.CompleteMultipartUploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		MultipartUpload: &s3.CompletedMultipartUpload{
+			Parts: fileParts,
+		},
+		UploadId: aws.String(uploadId),
+	})
+	return err
+}
+
+// Upload an entire file to remote AWS S3 storage.
+func (u *Uploader) SimpleUpload(key string, body []byte) error {
+	_, err := u.s3Uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader(body),
+	})
+	return err
+}
+
+// Upload a file part to remote AWS S3 storage.
+func (u *Uploader) UploadPart(key, uploadId string, body []byte, length, partNumber int64) (*entity.Part, error) {
+	out, err := u.s3Uploader.S3.UploadPart(&s3.UploadPartInput{
+		Body:          bytes.NewReader(body),
+		Bucket:        aws.String(bucket),
+		ContentLength: aws.Int64(length),
+		Key:           aws.String(key),
+		PartNumber:    aws.Int64(partNumber),
+		UploadId:      aws.String(uploadId),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &entity.Part{ETag: *out.ETag, PartNumber: partNumber}, nil
+}
