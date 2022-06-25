1c861a9d3788fbeeb2db66ac1b9b468351014a47
mongche <mondaydarknight@gmail.com>
2022-06-25 18:54:07 +0800

    feat: add interface to help unit tests

--- internal/app/controller.go
+++ internal/app/controller.go
@@ -8,13 +8,11 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/molpadia/molpastream/internal/domain/entity"
+	"github.com/molpadia/molpastream/internal/domain/repository"
 	"github.com/molpadia/molpastream/internal/httprange"
-	"github.com/molpadia/molpastream/internal/infrastructure"
-	"github.com/molpadia/molpastream/internal/infrastructure/persistence"
 )
 
 const (
@@ -22,17 +20,20 @@ const (
 	maxUploadChunkSize = 10 << 20
 )
 
+type controller struct {
+	video_repo repository.VideoRepository
+	uploader   repository.Uploader
+}
+
 // Get a single video.
-func getVideo(w http.ResponseWriter, r *http.Request) error {
+func (c *controller) getVideo(w http.ResponseWriter, r *http.Request) error {
 	id := mux.Vars(r)["id"]
 	if id == "" {
 		return &appError{http.StatusBadRequest, "video ID must be required"}
 	}
-	sess := session.Must(session.NewSession())
-	repo := persistence.NewVideoRepository(sess)
-	video, err := repo.GetById(id)
+	video, err := c.video_repo.GetById(id)
 	if err != nil {
-		return fmt.Errorf("failed to retrieve the video file")
+		return &appError{http.StatusInternalServerError, err.Error()}
 	}
 	if video == nil {
 		return &appError{http.StatusNotFound, "video ID does not exist"}
@@ -41,10 +42,10 @@ func getVideo(w http.ResponseWriter, r *http.Request) error {
 }
 
 // Create a new video.
-func createVideo(w http.ResponseWriter, r *http.Request) error {
+func (c *controller) createVideo(w http.ResponseWriter, r *http.Request) error {
 	var data VideoRequest
 	if err := parseJSON(w, r, &data); err != nil {
-		return fmt.Errorf("cannot parse JSON from request body: %v", err)
+		return &appError{http.StatusBadRequest, fmt.Sprintf("cannot parse JSON from request body: %v", err)}
 	}
 	if r.Header.Get("X-Upload-Content-Type") == "" {
 		return &appError{http.StatusBadRequest, "X-Upload-Content-Type header must be required"}
@@ -53,9 +54,6 @@ func createVideo(w http.ResponseWriter, r *http.Request) error {
 	if err != nil {
 		return &appError{http.StatusBadRequest, "X-Upload-Content-Length header must be required"}
 	}
-	sess := session.Must(session.NewSession())
-	uploader := infrastructure.NewUploader(sess)
-	repo := persistence.NewVideoRepository(sess)
 	// Create a new video entity for persistence data store.
 	video := entity.NewVideo(
 		uuid.New().String(),
@@ -69,23 +67,23 @@ func createVideo(w http.ResponseWriter, r *http.Request) error {
 	switch r.URL.Query().Get("uploadType") {
 	case "media":
 	case "resumable":
-		uploadId, err := uploader.CreateMultipart(video.Id)
+		uploadId, err := c.uploader.CreateMultipart(video.Id)
 		if err != nil {
-			return fmt.Errorf("failed to create multipart upload: %v", err)
+			return &appError{http.StatusInternalServerError, err.Error()}
 		}
 		video.NewUpload(uploadId)
 	default:
 		return &appError{http.StatusBadRequest, "Invalid upload type"}
 	}
 	// Save the multipart file information to the persistence.
-	if err = repo.Save(video); err != nil {
-		return fmt.Errorf("failed to save data to dynamodb: %v", err)
+	if err = c.video_repo.Save(video); err != nil {
+		return &appError{http.StatusInternalServerError, err.Error()}
 	}
 	return replyJSON(w, VideoResponse{video.Id, video.Description, video.Tags, video.Metadata, video.Status}, http.StatusCreated)
 }
 
 // Upload the video to the remote storage.
-func uploadVideo(w http.ResponseWriter, r *http.Request) error {
+func (c *controller) uploadVideo(w http.ResponseWriter, r *http.Request) error {
 	id := mux.Vars(r)["id"]
 	if id == "" {
 		return &appError{http.StatusBadRequest, "video ID must be required"}
@@ -102,12 +100,9 @@ func uploadVideo(w http.ResponseWriter, r *http.Request) error {
 		return &appError{http.StatusBadRequest, fmt.Sprintf("size must be the multiple of %d bytes", minUploadChunkSize)}
 	}
 	var cr *httprange.ContentRange
-	sess := session.Must(session.NewSession())
-	uploader := infrastructure.NewUploader(sess)
-	repo := persistence.NewVideoRepository(sess)
-	video, err := repo.GetById(id)
+	video, err := c.video_repo.GetById(id)
 	if err != nil {
-		return fmt.Errorf("failed to retrieve the video file from DynamoDB: %v", err)
+		return &appError{http.StatusInternalServerError, err.Error()}
 	}
 	if video == nil {
 		return &appError{http.StatusNotFound, "video ID does not exist"}
@@ -116,7 +111,7 @@ func uploadVideo(w http.ResponseWriter, r *http.Request) error {
 	if r.Header.Get("Content-Range") != "" {
 		cr, err = httprange.ParseContentRange(r.Header.Get("Content-Range"))
 		if err != nil {
-			return fmt.Errorf("cannot parse Content-Range header: %v", err)
+			return &appError{http.StatusInternalServerError, err.Error()}
 		}
 		if cr.Length() != size {
 			return &appError{http.StatusBadRequest, "invalid length of Content-Range header"}
@@ -128,39 +123,39 @@ func uploadVideo(w http.ResponseWriter, r *http.Request) error {
 	buf := new(bytes.Buffer)
 	body := http.MaxBytesReader(w, r.Body, maxUploadChunkSize)
 	if _, err = io.Copy(buf, body); err != nil {
-		return fmt.Errorf("failed to write binary data: %v", err)
+		return &appError{http.StatusInternalServerError, err.Error()}
 	}
 	// Upload the video file by the given upload type.
 	// - media: Simple upload. Use this type to quickly transfer small media file to the remote storage.
 	// - resumable: Resumable upload. Use this type for large files when there's a high chance fo network interruption.
 	switch r.URL.Query().Get("uploadType") {
 	case "media":
-		err = uploader.SimpleUpload(id, buf.Bytes())
+		err = c.uploader.SimpleUpload(id, buf.Bytes())
 		if err != nil {
-			return fmt.Errorf("failed to upload video to S3: %v", err)
+			return &appError{http.StatusInternalServerError, err.Error()}
 		}
 		video.SetStatus(entity.UploadedStatusCompleted)
-		if err = repo.Save(video); err != nil {
-			return fmt.Errorf("failed to save video to the persistence")
+		if err = c.video_repo.Save(video); err != nil {
+			return &appError{http.StatusInternalServerError, err.Error()}
 		}
 	case "resumable":
-		if cr != nil {
+		if cr == nil {
 			return &appError{http.StatusBadRequest, "Content-Range must be required"}
 		}
-		part, err := uploader.UploadPart(id, video.Upload.Id, buf.Bytes(), size, cr.CurrentPart())
+		part, err := c.uploader.UploadPart(id, video.Upload.Id, buf.Bytes(), size, cr.CurrentPart())
 		if err != nil {
-			return fmt.Errorf("failed to partial upload to S3, %v", err)
+			return &appError{http.StatusInternalServerError, err.Error()}
 		}
 		video.AddUploadPart(part)
 		// Assemble uploaded parts and complete the upload.
 		if len(video.Upload.Parts) >= int(cr.Parts()) {
 			video.SetStatus(entity.UploadedStatusCompleted)
-			if err = uploader.CompleteMultipart(id, video.Upload.Id, video.Upload.Parts); err != nil {
-				return fmt.Errorf("failed to complete multipart upload: %v", err)
+			if err = c.uploader.CompleteMultipart(id, video.Upload.Id, video.Upload.Parts); err != nil {
+				return &appError{http.StatusInternalServerError, err.Error()}
 			}
 		}
-		if err = repo.Save(video); err != nil {
-			return fmt.Errorf("failed to save video to the persistence")
+		if err = c.video_repo.Save(video); err != nil {
+			return &appError{http.StatusInternalServerError, err.Error()}
 		}
 	default:
 		return &appError{http.StatusBadRequest, "Invalid upload type"}
